500bcfc7a55ca90f5a42b3893038b84d500e70a0
jmjmjames <chungjm0711@gmail.com>
2022-08-02 11:15:38 +0900

    [Feat] : Annotation 추가 및 적용

--- src/main/java/com/exam/article/ArticleController.java
+++ src/main/java/com/exam/article/ArticleController.java
@@ -1,4 +1,12 @@
 package com.exam.article;
 
+import com.exam.annotation.Controller;
+import com.exam.annotation.GetMapping;
+
+@Controller
 public class ArticleController {
+    @GetMapping("/usr/article/list")
+    public void list() {
+
+    }
 }
